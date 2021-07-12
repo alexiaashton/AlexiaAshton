2525ff5edf082edfa4c865eb61c4320e3b9439ae
Alexia Ashton <ashtonalexia@gmail.com>
2021-07-12 13:25:02 +0200

    adding portfolio pages

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__Link');
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
-})
+});
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
