903bf221cbfc8b919fbe2ccbc70fb00b06549d4b
Alexia Ashton <ashtonalexia@gmail.com>
2021-07-12 12:52:38 +0200

    wroking on toggle aspect

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__Link');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 })
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
+
